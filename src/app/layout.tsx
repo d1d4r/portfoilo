cc70889608152a119598176f3103ed86a1a8f5d9
didar <deddarnawzad@gmail.com>
2024-04-27 19:49:29 +0300

    change import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navigations from "@/components/Navigations";
-import Grid from "@/components/Grid";
-
-
+import Navigations from "../components/Navigations";
+import Grid from "../components/Grid"
 
 const inter = Inter({ subsets: ["latin"] });
 
